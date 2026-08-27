72b69f2d7236a45834e87c2b61b9a45f6ec2388d
EsterIso <esterlinjpaulino@gmail.com>
2026-08-27 05:20:34 -0400

    changes in dark mode and filter implementation

--- nightshift.js
+++ nightshift.js
@@ -1,40 +1,49 @@
 let darkModeActive = false;
 let filterActive = false;
 
-function activate(allElements){
+function activate(view, allElements){
   if (filterActive) {
     filterActive = !filterActive;
   }
   darkModeActive = !darkModeActive;
-  allElements.forEach(element => changeStyle(element));
+  view.forEach(view => changeStyle(view))
+  allElements.forEach(element => {
+    const bg = getComputedStyle(element).backgroundImage;
+    if (bg && bg !== "none"){
+      changeStyle(element)
+    }
+  });
   function changeStyle(el) {
-    el.className = darkModeActive ? "night-shift" : "none"
+    if (el.tagName === 'HTML'){
+      el.className = darkModeActive ? "night-shift" : "none"
+    }
     el.style.filter = darkModeActive ? "invert(100%) hue-rotate(180deg)" : "none";
+    }
   }
-}
 
-function filter(allElements){
+function filter(view, allElements){
   if (darkModeActive){
     darkModeActive = !darkModeActive;
   }
   filterActive = !filterActive;
-  allElements.forEach(element => changeFilter(element));
+  view.forEach(element => changeFilter(element));
   function changeFilter(el) {
     el.className = "none";
-    el.style.filter = filterActive ? "sepia(20%) saturate(140%) contrast(105%)" : "none";
+    el.style.filter = filterActive ? "sepia(35%) brightness(90%)" : "none";
   }
   
 }
 
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  const allElements = document.querySelectorAll('html');
+  const view = document.querySelectorAll('html');
+  const allElements = document.querySelectorAll('*');
   if (message.type === "activate") {
-    activate(allElements);
+    activate(view, allElements);
     // Send a response back synchronously
     sendResponse({ reply: "Active Darkmode" });
   }
   else if (message.type === "filter"){
-    filter(allElements);
+    filter(view, allElements);
     sendResponse({ reply: "Active Filter" });
   }
 });
